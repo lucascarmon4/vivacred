757bfe7573d64036ed3e0d29fb4703676efa60e1
Lucas Carmona <lucascarmonaneto510@gmail.com>
2022-06-26 21:56:09 -0300

    falta responsivo 100%

--- assets/script/servicesSm.js
+++ assets/script/servicesSm.js
@@ -6,6 +6,7 @@ let square3 = document.querySelector('.square3');
 let square4 = document.querySelector('.square4');
 let square5 = document.querySelector('.square5');
 let square6 = document.querySelector('.square6');
+let sectionSquare = document.querySelector('.hide-services');
 let divSquares = document.querySelector('.div-squares');
 
 let squaresX = document.querySelectorAll('.square-x');
@@ -15,7 +16,7 @@ for (let i = 0; i < links.length; i++) {
     link.addEventListener('click', (event) => {
         let clicked = event.target;
         divSquares.classList.remove('hidden');
-
+        sectionSquare.classList.remove('hidden');
 
         if (clicked.id == 'link1') {
             square1.classList.remove('hidden');
@@ -47,24 +48,41 @@ for (let i = 0; i < links.length; i++) {
         divSquares.addEventListener('click', (event) => {
             const clicked = event.target;
             if (clicked.className == 'div-squares' || clicked.className == 'square-x' || clicked.id == 'close1' || clicked.id == 'close2') {
-                square1.classList.add('hidden');
-                square2.classList.add('hidden');
-                square3.classList.add('hidden');
-                square4.classList.add('hidden');
-                square5.classList.add('hidden');
-                square6.classList.add('hidden');
-                square1.classList.remove('show-service');
-                square2.classList.remove('show-service');
-                square3.classList.remove('show-service');
-                square4.classList.remove('show-service');
-                square5.classList.remove('show-service');
-                square6.classList.remove('show-service');
-                divSquares.classList.add('hidden');
-            }
-            console.log(clicked);
+                divSquares.classList.add('div-squares-close');
+                square1.classList.add('squares-close');
+                square2.classList.add('squares-close');
+                square3.classList.add('squares-close');
+                square4.classList.add('squares-close');
+                square5.classList.add('squares-close');
+                square6.classList.add('squares-close');
+                setTimeout(() => {
+                    square1.classList.add('hidden');
+                    square2.classList.add('hidden');
+                    square3.classList.add('hidden');
+                    square4.classList.add('hidden');
+                    square5.classList.add('hidden');
+                    square6.classList.add('hidden'); 
 
+                    square1.classList.remove('show-service');
+                    square2.classList.remove('show-service');
+                    square3.classList.remove('show-service');
+                    square4.classList.remove('show-service');
+                    square5.classList.remove('show-service');
+                    square6.classList.remove('show-service');
+
+                    square1.classList.remove('squares-close'); //animation
+                    square2.classList.remove('squares-close'); //animation
+                    square3.classList.remove('squares-close'); //animation
+                    square4.classList.remove('squares-close'); //animation
+                    square5.classList.remove('squares-close'); //animation
+                    square6.classList.remove('squares-close'); //animation
+
+                    divSquares.classList.add('hidden');
+                    divSquares.classList.remove('div-squares-close');
+                    
+                    sectionSquare.classList.add('hidden');
+                }, 500);
+            }
         })
     })
-
-
 }
